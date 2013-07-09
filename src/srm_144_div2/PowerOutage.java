ec379a2b652df57730a2c9a7bc04799b2a0dc3da
Hao Ren <github.claravista@gmail.com>
2013-07-09 18:46:42 +0200

    add week1.question3 sketch

---
+++ src/srm_144_div2/PowerOutage.java
@@ -0,0 +1,17 @@
+package srm_144_div2;
+
+public class PowerOutage {
+
+	public int estimateTimeOut(vector <int> fromJunction, vector <int> toJunction, vector <int> ductLength) {
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
