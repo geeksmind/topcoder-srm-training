e21b2aaa6d71e4d264acee31ce1b666ac117b252
Hao Ren <github.claravista@gmail.com>
2013-07-10 11:37:16 +0200

    rename package

---
+++ src/srm_144_div2/araragi/PowerOutage.java
@@ -0,0 +1,25 @@
+package srm_144_div2.araragi;
+
+/**
+ * Class: PowerOutage
+ * Method: estimateTimeOut
+ * Parameters: int[], int[], int[]
+ * Returns: int Method signature: int estimateTimeOut(int[] fromJunction, int[] toJunction, int[] ductLength)
+ * (be sure your method is public)
+ */
+
+public class PowerOutage {
+
+    public int estimateTimeOut(int[] fromJunction, int[] toJunction, int[] ductLength) {
+	return 0;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
