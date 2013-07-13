e9f582b64972af63f47787491a257ce29b818465
geeksmind <redocpot@geeksmind.net>
2013-07-13 02:34:42 +0200

    add 144 div1 question2 solution

--- src/srm_144_div1/araragi/Lottery.java
+++ src/srm_144_div1/araragi/Lottery.java
@@ -1,8 +1,6 @@
 package srm_144_div1.araragi;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Class: Lottery
@@ -18,49 +16,48 @@ public class Lottery {
     class Game {
 	private String m_name;
 	private double m_nbValidTicket;
+	double[][] matrix = new double[100][8];
 
-	public double rec(int root, int depth, double[][] mat) {
+	public double rec(int root, int depth) {
 	    double sum = 0;
 	    if (depth == 1)
 		return (double) root;
 
-	    if (mat[root-1][depth-1] != -1)
-		return mat[root-1][depth-1];
+	    if (matrix[root - 1][depth - 1] != -1)
+		return matrix[root - 1][depth - 1];
 
 	    for (int i = 1; i <= root; i++) {
-		sum += rec(i, depth - 1, mat);
+		sum += rec(i, depth - 1);
 	    }
 
-	    mat[root-1][depth-1] = sum;
+	    matrix[root - 1][depth - 1] = sum;
 	    return sum;
 	}
 
-	public int fact(int n) {
+	public double fact(int n) {
 	    return n == 1 || n == 0 ? 1 : n * fact(n - 1);
 	}
 
-	public int perm(int n, int r) {
+	public double perm(int n, int r) {
 	    return fact(n) / fact(n - r);
 	}
 
-	public int comb(int n, int r) {
+	public double comb(int n, int r) {
 	    return fact(n) / fact(r) / fact(n - r);
 	}
 
 	Game(String name, int choices, int blanks, boolean sorted, boolean unique) {
-	    
-	    double[][] matrix = new double[100][8];
-	    
-	    for (int row=0; row < 100; row++) {
-		      for (int col=0; col < 8; col++) {
-		        matrix[row][col] = -1;
-		      }
-		    }
+
+	    for (int row = 0; row < 100; row++) {
+		for (int col = 0; col < 8; col++) {
+		    matrix[row][col] = -1;
+		}
+	    }
 	    m_name = name;
 	    if (!sorted && !unique) {
-		m_nbValidTicket = (int) Math.pow(choices, blanks);
+		m_nbValidTicket = (double) Math.pow(choices, blanks);
 	    } else if (sorted && !unique) {
-		m_nbValidTicket = rec(choices, blanks, matrix);
+		m_nbValidTicket = rec(choices, blanks);
 	    } else if (!sorted && unique) {
 		m_nbValidTicket = perm(choices, blanks);
 	    } else if (sorted && unique) {
@@ -70,6 +67,10 @@ public class Lottery {
 
 	public double getNbValidTicket() {
 	    return m_nbValidTicket;
+	}
+
+	public String getName() {
+	    return m_name;
 	}
     }
 
@@ -81,16 +82,50 @@ public class Lottery {
 	    String nm = pair[0];
 	    String[] opts = pair[1].split(" ");
 	    gameList[i] = new Game(nm, Integer.parseInt(opts[0]), Integer.parseInt(opts[1]), opts[2].equals("T") ? true : false, opts[3].equals("T") ? true : false);
-	    System.out.println(gameList[i].getNbValidTicket());
+	    // System.out.println(gameList[i].getName() + " <==> " +
+	    // gameList[i].getNbValidTicket());
 	}
 
-	return null;
+	for (int i = 0; i < gameList.length; i++) {
+	    for (int j = 0; j < gameList.length - i - 1; j++) {
+		if (gameList[j].getNbValidTicket() > gameList[j + 1].getNbValidTicket()) {
+		    Game temp = gameList[j + 1];
+		    gameList[j + 1] = gameList[j];
+		    gameList[j] = temp;
+		}
+		if (gameList[j].getNbValidTicket() == gameList[j + 1].getNbValidTicket()) {
+		    if (gameList[j].getName().compareTo(gameList[j + 1].getName()) > 0) {
+			Game temp = gameList[j + 1];
+			gameList[j + 1] = gameList[j];
+			gameList[j] = temp;
+		    }
+		}
+	    }
+	}
+
+	String[] res = new String[gameList.length];
+	for (int i = 0; i < gameList.length; i++) {
+	    res[i] = gameList[i].getName();
+	}
+
+	return res;
     }
 
     public static void main(String[] args) {
-	String[] rules = new String[] { "PICK ANY TWO: 10 2 F F", "PICK TWO IN ORDER: 93 8 T F", "PICK TWO DIFFERENT: 10 2 F T", "PICK TWO LIMITED: 10 2 T T" };
+	String[] rules = new String[] {
+		"PICK ANY TWO: 10 2 F F",
+		"PICK TWO IN ORDER: 10 2 T F",
+		"PICK TWO DIFFERENT: 10 2 F T",
+		"PICK TWO LIMITED: 10 2 T T" };
+	// String[] rules = new String[] {
+	// "INDIGO: 93 8 T F",
+	// "ORANGE: 29 8 F T",
+	// "VIOLET: 76 6 F F",
+	// "BLUE: 100 8 T T",
+	// "RED: 99 8 T T",
+	// "GREEN: 78 6 F T",
+	// "YELLOW: 75 6 F F" };
 
-	System.out.println(new Lottery().sortByOdds(rules));
-//	System.out.println(new Lottery().rec(93, 8));
+	System.out.println(Arrays.toString(new Lottery().sortByOdds(rules)));
     }
 }
