9792754c29eb41f0257f51b70d713915087ad96e
geeksmind <redocpot@geeksmind.net>
2013-07-13 03:04:44 +0200

    add 144 div1 q3 sketch

---
+++ src/srm_144_div1/araragi/PenLift.java
@@ -0,0 +1,22 @@
+package srm_144_div1.araragi;
+
+/**
+ * Class: PenLift
+ * Method: numTimes
+ * Parameters: String[], int
+ * Returns: int
+ * Method signature: int numTimes(String[] segments, int n)
+ * (be sure your method is public)
+ */
+
+public class PenLift {
+
+    int numTimes(String[] segments, int n) {
+	return 0;
+    }
+
+    public static void main(String[] args) {
+	String[] seg = new String[] { "-10 0 10 0", "0 -10 0 10" };
+	System.out.println(new PenLift().numTimes(seg, 1));
+    }
+}
